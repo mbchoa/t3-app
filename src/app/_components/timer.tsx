3eae99af38e8ab6f3c6c7686280d293751dc153f
Michael-Bryant Choa <mbchoa@gmail.com>
2024-01-07 17:07:04 -0800

    commit

---
+++ src/app/_components/timer.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function Timer() {
+  const [count, setCount] = useState(0);
+
+  // create a timer
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div className="flex items-center justify-center">
+        <div className="text-4xl font-bold">{count}</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { CreatePost } from "@/app/_components/create-post";
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <CreatePost />
+    </main>
+  );
+}
